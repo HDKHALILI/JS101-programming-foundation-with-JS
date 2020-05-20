9634930c6c683ad0a3ccb771e9ab376effb2f03b
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-05-20 15:00:46 +1000

    modify rock paper scissors to include lizard and spock

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,24 +1,53 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const colors = require('colors');
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
+const RESULTS = [
+  ['rock', 'scissors', 'lizard'],
+  ['scissors', 'paper', 'lizard'],
+  ['paper', 'rock', 'spock'],
+  ['spock', 'rock', 'scissors'],
+  ['lizard', 'paper', 'spock']
+];
+const MESSAGES = {
+  rock: {
+    scissors: "Rock crushed the Scissors",
+    lizard: "Rock crushed the Lizard"
+  },
+  scissors: {
+    paper: "Scissors cut the Paper",
+    lizard: "Scissors decaptitated the Lizard"
+  },
+  paper: {
+    rock: "Paper covered the Rock",
+    spock: "Paper disproved Spock"
+  },
+  spock: {
+    rock: "Spock vaporized the Rock",
+    scissors: "Spock smashed the Scissors"
+  },
+  lizard: {
+    paper: "Lizard ate the Paper",
+    spock: "Lizard poisoned Spock"
+  }
+
+};
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function displayWinner(choice, computerChoice) {
+function displayWinner(choice, computerChoice, results, messages) {
   prompt(`You chose ${choice}, computer chose ${computerChoice}`);
-
-  if ((choice === 'rock' && computerChoice === 'scissors') ||
-    (choice === 'paper' && computerChoice === 'rock') ||
-    (choice === 'scissors' && computerChoice === 'paper')) {
-    prompt('You win!');
-  } else if ((choice === 'rock' && computerChoice === 'paper') ||
-    (choice === 'paper' && computerChoice === 'scissors') ||
-    (choice === 'scissors' && computerChoice === 'rock')) {
-    prompt('Computer wins!');
-  } else {
-    prompt("It's a tie");
+  for (let index = 0; index < results.length; index += 1) {
+    if (choice === results[index][0] &&
+      results[index].slice(1).includes(computerChoice)) {
+      return `*** You Win! *** ${messages[choice][computerChoice]}`.bold.green;
+    } else if (computerChoice === results[index][0] &&
+      results[index].slice(1).includes(choice)) {
+      return `*** Computer Win! *** ${messages[computerChoice][choice]}`.bold.red;
+    }
   }
+  return "*** It's a tie ***".bold.bgBlue;
 }
 
 while (true) {
@@ -26,14 +55,14 @@ while (true) {
   let choice = readline.question('> ');
 
   while (!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice");
+    prompt("That's not a valid choice".red);
     choice = readline.question('> ');
   }
 
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
-  displayWinner(choice, computerChoice);
+  prompt(displayWinner(choice, computerChoice, RESULTS, MESSAGES));
 
   prompt('Do you want to play again (y/n)?');
   let answer = readline.question('> ');
@@ -43,4 +72,7 @@ while (true) {
   }
 
   if (answer[0] !== 'y') break;
+
+  let lineLength = `Choose one: ${VALID_CHOICES.join(', ')}`.length;
+  prompt(colors.green(`${'-'.repeat(lineLength)}`));
 }
