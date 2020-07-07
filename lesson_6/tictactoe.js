707950612ce0007982840db6d550564153089972
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-07-07 14:01:37 +1000

    add setup and display board step to tictactoe.js

---
+++ lesson_6/tictactoe.js
@@ -0,0 +1,39 @@
+function displayBoard(board) {
+  console.log('');
+  console.log('     |     |');
+  console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}`);
+  console.log('     |     |');
+  console.log('-----+-----+-----');
+  console.log('     |     |');
+  console.log(`  ${board['4']}  |  ${board['5']}  |  ${board['6']}`);
+  console.log('     |     |');
+  console.log('-----+-----+-----');
+  console.log('     |     |');
+  console.log(`  ${board['7']}  |  ${board['8']}  |  ${board['9']}`);
+  console.log('     |     |');
+  console.log('');
+}
+
+// let board = {
+//   1: 'X', // top left
+//   2: ' ', // top center
+//   3: ' ', // top right
+//   4: ' ', // middle left
+//   5: 'O', // center
+//   6: ' ', // middle right
+//   7: ' ', // bottom left
+//   8: ' ', // bottom center
+//   9: 'X', // bottom right
+// };
+
+function initialiseBoard() {
+  let board = {};
+  for (let square = 1; square <= 9; square += 1) {
+    board[square] = ' ';
+  }
+
+  return board;
+}
+
+let board = initialiseBoard();
+displayBoard(board);
