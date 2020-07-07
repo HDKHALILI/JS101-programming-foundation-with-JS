80a2a591f369db0e2af00c2f9ecc7edefc2c1cd9
HDKHALILI <14176079+HDKHALILI@users.noreply.github.com>
2020-07-07 19:40:08 +1000

    add determining winner functionality to tictactoe.js

--- lesson_6/tictactoe.js
+++ lesson_6/tictactoe.js
@@ -1,37 +1,28 @@
-const readline = require('readline-sync');
+const readline = require("readline-sync");
 
-const INITIAL_MARKER = ' ';
-const HUMAN_MARKER = 'X';
-const COMPUTER_MARKER = 'O';
+const INITIAL_MARKER = " ";
+const HUMAN_MARKER = "X";
+const COMPUTER_MARKER = "O";
 
 function displayBoard(board) {
   console.clear();
-  console.log('');
-  console.log('     |     |');
-  console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}`);
-  console.log('     |     |');
-  console.log('-----+-----+-----');
-  console.log('     |     |');
-  console.log(`  ${board['4']}  |  ${board['5']}  |  ${board['6']}`);
-  console.log('     |     |');
-  console.log('-----+-----+-----');
-  console.log('     |     |');
-  console.log(`  ${board['7']}  |  ${board['8']}  |  ${board['9']}`);
-  console.log('     |     |');
-  console.log('');
-}
 
-// let board = {
-//   1: 'X', // top left
-//   2: ' ', // top center
-//   3: ' ', // top right
-//   4: ' ', // middle left
-//   5: 'O', // center
-//   6: ' ', // middle right
-//   7: ' ', // bottom left
-//   8: ' ', // bottom center
-//   9: 'X', // bottom right
-// };
+  console.log(`You are ${HUMAN_MARKER}. Computer is ${COMPUTER_MARKER}`);
+
+  console.log("");
+  console.log("     |     |");
+  console.log(`  ${board["1"]}  |  ${board["2"]}  |  ${board["3"]}`);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${board["4"]}  |  ${board["5"]}  |  ${board["6"]}`);
+  console.log("     |     |");
+  console.log("-----+-----+-----");
+  console.log("     |     |");
+  console.log(`  ${board["7"]}  |  ${board["8"]}  |  ${board["9"]}`);
+  console.log("     |     |");
+  console.log("");
+}
 
 function initialiseBoard() {
   let board = {};
@@ -47,7 +38,7 @@ function prompt(message) {
 }
 
 function emptySquares(board) {
-  return Object.keys(board).filter(key => {
+  return Object.keys(board).filter((key) => {
     return board[key] === INITIAL_MARKER;
   });
 }
@@ -56,7 +47,7 @@ function playerChoosesSquare(board) {
   let square; // declared here so we can use it outside the loop
 
   while (true) {
-    prompt(`Choose a square (${emptySquares(board).join(', ')}):`);
+    prompt(`Choose a square (${emptySquares(board).join(", ")}):`);
     square = readline.question().trim();
 
     if (emptySquares(board).includes(square)) break;
@@ -77,18 +68,66 @@ function computerChoosesSquare(board) {
 function boardFull(board) {
   return emptySquares(board).length === 0;
 }
+const WINNING_LINES = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9], // rows
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9], // columns
+  [1, 5, 9],
+  [3, 5, 7], // diagonals
+];
 
-function someoneWon(board) {
-  return false;
+function detectWinner(board) {
+  for (let line = 0; line < WINNING_LINES.length; line += 1) {
+    let [sq1, sq2, sq3] = WINNING_LINES[line];
+    if (
+      board[sq1] === HUMAN_MARKER &&
+      board[sq2] === HUMAN_MARKER &&
+      board[sq3] === HUMAN_MARKER
+    ) {
+      return "Player";
+    } else if (
+      board[sq1] === COMPUTER_MARKER &&
+      board[sq2] === COMPUTER_MARKER &&
+      board[sq3] === COMPUTER_MARKER
+    ) {
+      return "Computer";
+    }
+  }
+
+  return null;
 }
 
-let board = initialiseBoard();
-displayBoard(board);
+function someoneWon(board) {
+  // !! convert truthy values to true
+  //    converts undefined and null to false
+  return !!detectWinner(board);
+}
 
 while (true) {
-  playerChoosesSquare(board);
-  computerChoosesSquare(board);
+  let board = initialiseBoard();
+
+  while (true) {
+    displayBoard(board);
+
+    playerChoosesSquare(board);
+    if (someoneWon(board) || boardFull(board)) break;
+
+    computerChoosesSquare(board);
+    if (someoneWon(board) || boardFull(board)) break;
+  }
+
   displayBoard(board);
 
-  if (someoneWon(board) || boardFull(board)) break;
+  if (someoneWon(board)) {
+    prompt(`${detectWinner(board)} won!`);
+  } else {
+    prompt("It's a tie!");
+  }
+
+  prompt("Play again? (y or n)");
+  let answer = readline.question().toLowerCase()[0];
+  if (answer !== "y") break;
 }
